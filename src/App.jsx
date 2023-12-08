5e0828677d891f2a5675af5ebc7d52b142a1de03
Kiran <145902144+KiranGadhavi@users.noreply.github.com>
2023-12-08 12:41:43 +0000

    add redx project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+
+  return (
+    < >
+     <h1>Redux Toolkit</h1>
+     <AddTodo/>
+     <Todos/>
+    </>
+  )
+}
+
+export default App
